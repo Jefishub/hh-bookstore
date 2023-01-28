662aefd2a3f2c61e0bb4166f3990c256727f90f7
Jere Manni <jere.manni@gmail.com>
2023-01-28 19:38:43 +0200

    first exercise

---
+++ src/main/java/fi/haagahelia/Bookstore/web/BookController.java
@@ -0,0 +1,16 @@
+package fi.haagahelia.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BookController {
+	
+	@GetMapping("/index") 
+	public String welcome() {
+		return "hello";
+	}
+
+}
