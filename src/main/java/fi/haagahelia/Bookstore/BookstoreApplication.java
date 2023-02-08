bb4b1d73069610324b85774c115d2558c4801a5a
Jere Manni <jere.manni@gmail.com>
2023-02-08 21:26:10 +0200

    chapter 3, 1 and 2

--- src/main/java/fi/haagahelia/Bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/Bookstore/BookstoreApplication.java
@@ -1,7 +1,12 @@
 package fi.haagahelia.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.Bookstore.domain.Book;
+import fi.haagahelia.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +15,14 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book book1 = new Book("Retkimatkat", "Johnson", "somerandom", 1999, 19.99);
+			Book book2 = new Book("Menovehkeet", "Johnson", "somerandom", 1999, 19.99);
+			
+			repository.save(book1);
+			repository.save(book2);
+		};
+	}
 }
--- src/main/java/fi/haagahelia/Bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/Bookstore/web/BookController.java
@@ -1,12 +1,25 @@
 package fi.haagahelia.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import fi.haagahelia.Bookstore.domain.BookRepository;
 
 
 @Controller
 public class BookController {
+	
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
 	
 	@GetMapping("/index") 
 	public String welcome() {
